92bcaa0e9ea50ebb7e301d712dae3f9a9a1d0780
Amy Holt <amy.elizabeth.holt@gmail.com>
2019-10-23 06:46:52 -0600

    Build functionality and styles - ready for refactor

--- main.js
+++ main.js
@@ -10,14 +10,22 @@ highKickBtn.addEventListener('click', selectHighKick);
 
 function selectPushUp(event) {
   activityType = "push-up";
-  highKickBtn.style.background = "white";
-  event.target.style.background = "blue";
+  highKickBtn.style.color = "black";
+  highKickBtn.style.borderColor = "#C1C0C0";
+  highKickBtn.style.backgroundColor = "white";
+  event.target.style.backgroundColor = "#DBFFE3";
+  event.target.style.color = "#00560C";
+  event.target.style.borderColor = "#DBFFE3";
 }
 
 function selectHighKick() {
   activityType = "high-kick";
-  pushUpBtn.style.background = "white";
-  event.target.style.background = "red";
+  pushUpBtn.style.color = "black";
+  pushUpBtn.style.borderColor = "#C1C0C0";
+  pushUpBtn.style.backgroundColor = "white";
+  event.target.style.backgroundColor = "#EBEBFF";
+  event.target.style.color = "#2B006B";
+  event.target.style.borderColor = "#EBEBFF";
 }
 
 function submitActivity(event) {
@@ -26,14 +34,14 @@ function submitActivity(event) {
 
   if (activityType === "push-up") {
     activityCards.innerHTML += `
-      <section class="card">
+      <section class="card push-up-card">
         <h2>Push Ups</h2>
         <time>${date}</time>
       </section>
     `;
   } else if (activityType === "high-kick") {
     activityCards.innerHTML += `
-      <section class="card">
+      <section class="card high-kick-card">
         <h2>High Kicks</h2>
         <time>${date}</time>
       </section>
